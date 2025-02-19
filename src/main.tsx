bf8b50e155002a869ae9b0443eea81102c12d7ec
Debashis95 <debashisroyjgec@gmail.com>
2025-02-19 16:57:05 +0530

    redirect

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider, Navigate } from "react-router";
+import { createBrowserRouter, RouterProvider, } from "react-router";
 import { Rsvp } from "./components/Rsvp.tsx";
 import { useState } from "react";
 
